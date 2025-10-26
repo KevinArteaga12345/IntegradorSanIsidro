78c1fa0f8b9a6e81b192f2e14fd8ac04a56afe55
Gabo1514 <zafirobello078@gmail.com>
2025-10-26 10:57:20 -0500

    Subo mi versión buena de IntegradorSanIsidro (ahora sin subrepositorio)

--- San-Isidro/src/main/java/com/SanIsidro/repository/AdministradorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.SanIsidro.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sanisidro.model.Administrador;
-
-public interface AdministradorRepository extends JpaRepository<Administrador, Long> {
-}
